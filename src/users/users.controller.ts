57b02287c0bbb81f1f22035f7f500dd0ee85bdb7
Bhairavi Gawas <bhairavigawas9@gmail.com>
2025-08-21 15:49:38 +0530

    feat: user module creattion

---
+++ src/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  @Post('signup')
+  create(@Body() createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+}
